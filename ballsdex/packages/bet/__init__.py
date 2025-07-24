40bbfa89f330b4c2f5d471c10aa49d6ece40dfee
paqueta-fbd <mommiamaa@gmail.com>
2025-07-24 15:35:56 +0200

    Create bet packages

---
+++ ballsdex/packages/bet/__init__.py
@@ -0,0 +1,11 @@
+from typing import TYPE_CHECKING
+
+from ballsdex.packages.bet.cog import Bet
+
+if TYPE_CHECKING:
+    from ballsdx.core.bot import BallsDexBot
+
+
+async def setup(bot: "BallsDexBot"):
+    await bot.add_cog(Bet(bot))
+  
